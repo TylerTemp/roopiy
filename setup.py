21630172892135048a626b6ba33f1539f93ce20d
TylerTemp <tylertempdev@gmail.com>
2024-05-11 21:49:02 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 from roopiy.version import __version__
 
 setup(
@@ -12,6 +12,7 @@ setup(
     keywords='deepfake',
     # url='',
     py_modules=['roopiy'],
+    packages=find_packages(),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     classifiers=[
